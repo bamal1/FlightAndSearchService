0b582419a636c72db2c794c8f82a917610b8a7d4
Your Name <you@example.com>
2023-07-03 23:10:11 +0530

    Added Airplanes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const {PORT} = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
 const db= require('./models/index');
-// const { City,Airport }= require('./models/index');
+// const { Airplane }= require('./models/index');
 
 
 
@@ -23,7 +23,7 @@ const setupAndStartServer = async () =>{
         if(process.env.SYNC_DB){
 
             db.sequelize.sync({alter: true});
-        }  
+        } 
     });
     
 }
